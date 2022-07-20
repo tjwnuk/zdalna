8d440f50225d06c674530d7fd2d6aa5c06d34fae
tjwnuk <tomasz.wnuk1@gmail.com>
2022-07-21 00:16:53 +0200

    Django 4.0.6 install

---
+++ board/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return HttpResponse('<h1>About site</h1> \n This is about site')
