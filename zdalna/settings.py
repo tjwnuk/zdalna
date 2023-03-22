ee2592d326361cbd300dc1a6fdf7687925ff2757
Tomasz Wnuk <tomasz.wnuk1@gmail.com>
2023-03-22 22:34:10 +0100

    move SECRET_KEY to .env file

--- zdalna/settings.py
+++ zdalna/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+# environmental variables
+import environ
+env = environ.Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#8!9ht)xik@tzxo*rb^xr9o#ob$nj!s$c)-hwye#iq7n5j4e=q'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
