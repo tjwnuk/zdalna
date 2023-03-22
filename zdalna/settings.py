6f326e02d53e96738949d159a383136161c87beb
Tomasz Wnuk <tomasz.wnuk1@gmail.com>
2023-03-22 22:50:23 +0100

    .env fix

--- zdalna/settings.py
+++ zdalna/settings.py
@@ -13,8 +13,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # environmental variables
+# remember to avoid spaces between variable name, = and value in .env file
+# SECRET_KEY = 1111 <- incorrect
+# SECRET_KEY=1111   <- correct
+
 import environ
 env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
