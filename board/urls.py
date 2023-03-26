3af56231dec3f05a744961cc6f6e8ff8124614ba
Tomasz Wnuk <tomasz.wnuk1@gmail.com>
2023-03-26 16:50:44 +0200

    simple detail view

--- board/urls.py
+++ board/urls.py
@@ -6,5 +6,5 @@ app_name = 'board'
 urlpatterns = [
     # post views
     path('', views.job_offers_list, name='job_offers_list'),
-    # path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:id>/', views.offer_detail, name='offer_detail'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from .models import JobOffer
 
 # Create your views here.
 
 def job_offers_list(request):
+    """
+    List of all job offers
+    """
     offers = JobOffer.objects.all()
     
     return render(request,
-                  'job_offers/detail.html',
+                  'job_offers/offers_list.html',
                   {'offers': offers })
+
+def offer_detail(request, id):
+    """
+    Details view of specific job offer
+    """
+
+    offer = get_object_or_404(JobOffer, id=id)
+    return render(request, 'job_offers/detail.html', {'offer': offer})
